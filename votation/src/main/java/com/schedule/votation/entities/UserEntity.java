fab7cbba4346ca962b3e85086bbeb48bffd5cb7c
Angela Talyta <tatazinhagms2@gmail.com>
2024-06-09 01:07:48 -0300

    Adicionando novas entidades no projeto

--- votation/src/main/java/com/schedule/votation/entities/UserEntity.java
+++ votation/src/main/java/com/schedule/votation/entities/UserEntity.java
@@ -1,38 +1,19 @@
 package com.schedule.votation.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public UserEntity() {
-    }
-
-    public UserEntity(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
