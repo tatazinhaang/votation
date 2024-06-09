4c49bbf1c22ce54649c3395ebb99584b3b20c654
Angela Talyta <tatazinhagms2@gmail.com>
2024-06-09 19:18:29 -0300

    CRUD no UserEntity

--- votation/src/main/java/com/schedule/votation/controller/UserController.java
+++ votation/src/main/java/com/schedule/votation/controller/UserController.java
@@ -2,22 +2,49 @@ package com.schedule.votation.controller;
 
 import com.schedule.votation.entities.UserEntity;
 import com.schedule.votation.repository.UserRepository;
+import com.schedule.votation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public List<UserEntity> getAllUser() {
-        return userRepository.findAll();
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
+        Optional<UserEntity> user = userService.findById(id);
+        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserEntity> updateUser(@PathVariable Long id, @RequestBody UserEntity user) {
+        return userService.update(id, user);
+    }
+
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
