dd4305837f35740fc06cf8337bd30ca65861ed17
Angela Talyta <tatazinhagms2@gmail.com>
2024-06-09 21:17:42 -0300

    Alterações no código, em Repository.

--- votation/src/main/java/com/schedule/votation/repository/ScheduleRepository.java
+++ votation/src/main/java/com/schedule/votation/repository/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.schedule.votation.repository;
 
 import com.schedule.votation.entities.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
 }
